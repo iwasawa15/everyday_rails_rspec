d8e2b6fd79a9eed43da48575f500c37ce7bcd9c9
iwasawa15 <tatsuya57sak@gmail.com>
2021-02-04 03:48:22 +0900

    ファイル自動生成の設定

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,9 @@ module Projects
     config.generators do |g|
       g.test_framework :rspec,
         view_specs: false,
-        helper_specs: false,
+        fixtures: false,        
+	view_specs: false,
+	helper_specs: false,
         routing_specs: false
     end
   end
