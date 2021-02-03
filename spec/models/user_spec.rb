f036212f8f8e719d8278b3383b158a87f60e29f4
iwasawa15 <tatsuya57sak@gmail.com>
2021-02-04 06:33:44 +0900

    user testの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'is valid with a first name, last name, email, and password'
+  it 'is valid with a first name, last name, email, and password' do
+    user = User.new(
+      first_name: 'Aaron',
+      last_name: 'Summer',
+      email: 'test@example.com',
+      password: 'fjdksauteijwsieiw-jfdks',
+    )
+    expect(user).to be_valid
+  end
 
-  it 'is invalid without a first name'
+  it 'is invalid without a first name' do
+    user = User.new(first_name: nil)
 
-  it 'is invalid without a last name'
+    user.valid?
+
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a last name' do
+    user = User.new(last_name: nil)
+
+    user.valid?
+
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
 
   it 'is invalid without an email address'
 
-  it 'is invalid with a duplicate email address'
+  it 'is invalid with a duplicate email address' do
+    User.create(
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: 'test@example.com',
+      password: 'kdjkhtaio-fdsk'
+    )
+
+    user = User.new(
+      first_name: 'Jane',
+      last_name: 'Tester',
+      email: 'test@example.com',
+      password: 'djkyhaihd-kukfds'
+    )
+
+    user.valid?
+
+    expect(user.errors[:email]).to include('has already been taken')
+  end
 
   it 'is return a user full name as a string'
 end
