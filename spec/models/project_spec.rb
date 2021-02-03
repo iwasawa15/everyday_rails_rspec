fdedeeb77456b8337fa99dc459931032f7654f42
iwasawa15 <tatsuya57sak@gmail.com>
2021-02-04 06:58:09 +0900

    project duplicate

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'does not allow duplicate project names per user' do
+    user = User.create(
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: 'test@example.com',
+      password: 'kdjkhtaio-fdsk'
+    )
+
+    user.projects.create(
+      name: 'Test Project'
+    )
+
+    new_project = user.projects.build(
+      name: 'Test Project'
+    )
+
+    new_project.valid?
+
+    expect(new_project.errors[:name]).to include('has already been taken')
+  end
+
+  it 'allows two users to share a project name' do
+    user = User.create(
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: 'test@example.com',
+      password: 'kdjkhtaio-fdsk'
+    )
+
+    user.projects.create(
+      name: 'Test Project'
+    )
+
+    other_user = User.create(
+      first_name: 'Jane',
+      last_name: 'Tester',
+      email: 'test2@example.com',
+      password: 'kdjkhtaio-fdsk'
+    )
+
+    other_project = other_user.projects.build(
+      name: 'Test Project'
+    )
+
+    expect(other_project).to be_valid
+  end
 end
